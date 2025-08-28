f67d0911b16363679ff47f28d406e20dc7f3f5cf
sinalpha <hanhdhx65@gmail.com>
2025-08-29 05:34:14 +0900

    add methods and variables to d3dcontroller

--- src/include/D3DController.h
+++ src/include/D3DController.h
@@ -6,6 +6,8 @@
 #include <DirectXMath.h>
 #include <d3dx12.h>
 
+
+
 class D3DController {
 public:
 
@@ -15,5 +17,50 @@ public:
 private:
 
 	void GetHardwareAdapter(IDXGIFactory2*, IDXGIAdapter1**);
+
+	bool InitFactory();
+	bool InitDevice();
+	bool InitCommandInterfaces();
+	bool InitFence();
+	bool InitFrameInterfaces();
+	bool InitSwapChain();
+	bool InitRenderTargetViewHeap();
+	bool InitRenderTarget();
+
+	void CreatePipeLine();
+	void SetViewPort();
+	void SetScissorRect();
+
+	void ClearBackBuffer();
+	void PresentBackBuffer();
+	void WaitDrawDone();
+	void SetCommandList();
+
+	static const UINT scmFrameCount{ 2 };
+	static const bool scmUseWarpDevice{ false };
+
+	Microsoft::WRL::ComPtr<ID3D12Device> mDevice;
+	Microsoft::WRL::ComPtr<IDXGIFactory4> mFactory;
+
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> mCommandAllocator;
+	Microsoft::WRL::ComPtr<ID3D12CommandQueue> mCommandQueue;
+	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> mCommandList;
+
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> mRootSignature;
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> mPipelineState;
+	CD3DX12_VIEWPORT mViewport;
+	CD3DX12_RECT mScissorRect;
+
+	Microsoft::WRL::ComPtr<ID3D12Fence> mFence;
+	HANDLE mFenceEvent{ NULL };
+	UINT64 mFenceValue{ 0 };
+
+	HRESULT mHr{ 0 };
+
+	Microsoft::WRL::ComPtr<ID3D12Resource> mRenderTargets[scmFrameCount];
+	Microsoft::WRL::ComPtr<IDXGISwapChain4> mSwapChain;
+	UINT mFrameIndex{ 0 };
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> mBackBufferViewHeap;
+	UINT mBackBufferViewDescriptorSize{ 0 };
 
 };
